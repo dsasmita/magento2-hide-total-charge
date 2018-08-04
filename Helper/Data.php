407ac74b621900c18caeb1856168efee64981ff9
dsasmita <dangs.work@gmail.com>
2018-08-04 10:49:22 +0700

    Create layout and templates to hide total charge

---
+++ Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dangs\TotalChargeHide\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+	public function __construct(
+        \Magento\Framework\App\Helper\Context $context
+    ) {
+        parent::__construct($context);
+    }
+
+	/**
+     * Return the warehouse address
+     *
+     * @return string
+     */
+
+	public function getStatusCharge(){
+		return $this->scopeConfig->getValue('sales/totals_sort/hide_charge', 
+                        \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+	}
+}
